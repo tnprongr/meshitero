4eef2cc92bf050c2dfa37156def3c8872c210e96
tnprongr <106970246+tnprongr@users.noreply.github.com>
2022-08-19 08:01:15 +0000

    [Finish]4-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,5 +1,13 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image = PostImage.new
+  end
+
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
@@ -10,4 +18,11 @@ class PostImagesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
 end
