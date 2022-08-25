ee4e0cf7ebc178c046803e018bd1e6e7d5498a57
tnprongr <106970246+tnprongr@users.noreply.github.com>
2022-08-25 16:43:20 +0000

    [Finish]4-22

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images 
+    @post_images = @user.post_images.page(params[:page])
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
